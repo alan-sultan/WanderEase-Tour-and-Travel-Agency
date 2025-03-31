d403288177cf66a95e60f64eb7e072a77cfd2a35
Ahmed Yassin <ahmedyasine230@gmail.com>
2025-03-31 22:27:46 +0300

    Update database.php

--- Backend/database.php
+++ Backend/database.php
@@ -1,11 +1,8 @@
 <?php
-$host = "localhost";
-$dbname = "wander_ease";
-$username = "root";  // Default in XAMPP
-$password = "";      // Leave empty in XAMPP
+require 'config.php';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
