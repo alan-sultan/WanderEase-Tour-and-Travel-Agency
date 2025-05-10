a4518f78c7538968066f25ab403d0dbefa5b9544
Alazar Zemene <149241148+alazem@users.noreply.github.com>
2025-05-10 21:03:42 +0300

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['user_id'])){
 require_once '../config.php'; // Include your database connection file
 
 // Check if the form is submitted
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // Retrieve form data
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
